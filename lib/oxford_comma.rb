b2775d13e4f12f050512ed330f7634a36fd9ba15
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-20 23:12:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -17,8 +17,7 @@ def oxford_comma(array)
       array.join
   elsif array.length == 2
       array.join(" and ")
-  else
-      lastStr = array.pop
+  else lastStr = array.pop
       array.join(", ") << ", and #{lastStr}"
   end
 end
