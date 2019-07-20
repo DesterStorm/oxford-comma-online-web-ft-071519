c8b2cbdbaa5383f621759e99553bf6c07408ccaf
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-20 23:03:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,4 @@
 
-# def oxford_comma(array)
-#   if array.length == 1
-#       array.join
-#   elsif array.length == 2
-#       array.join(" and ")
-#   else
-#       lastStr = array.pop
-#       array.join(", ") << ", and #{lastStr}"
-#   end
-# end
-
 
 def oxford_comma(array)
   if array.join 
