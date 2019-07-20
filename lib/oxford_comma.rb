f9b7aba185ccce45e260da2999236fb399de4fd4
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-20 23:12:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,12 +12,3 @@ def oxford_comma(array)
 end
 
 
-def oxford_comma(array)
-  if array.length == 1
-      array.join
-  elsif array.length == 2
-      array.join(" and ")
-  else lastStr = array.pop
-      array.join(", ") << ", and #{lastStr}"
-  end
-end
